b91154af07c862df4e9d4378fba4c72d96fc15ab
ricgnzlzcr <ricgnzlzcr@gmail.com>
2016-03-14 18:49:57 +0000

    Fixed path issue preventing Heroku working

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,9 @@ var app = express();
 
 //Load Angular application from 'public' folder
 var path = '../../public';
-app.use('/', express.static(path));
+app.use(express.static(__dirname + '/' + path));
 
 app.listen(process.env.PORT, function() {
+    console.log(__dirname + '/' + path);
     console.log("Find a dry cleaner server loaded");
 });
